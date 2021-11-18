855c090b6f1e258630dac6685cf625383c1c72d0
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-19 00:10:37 +0300

    Create error page

---
+++ layouts/error.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="container-fluid mt-5">
+      <h3 class="text-center">Something went wrong</h3>
+      <p class="text-center">This page not found :(</p>
+    </div>
+  </div>
+</template>
