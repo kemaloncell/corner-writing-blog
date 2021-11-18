03d8bca47ac4a2184a86c5bb79b05a0d7fd8e99f
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-18 11:31:30 +0300

    Construction of Post Detail Page

--- components/post/Post.vue
+++ components/post/Post.vue
@@ -4,7 +4,7 @@
       <h5 class="card-title">Card title</h5>
       <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-outline-info btn-sm">Show</a>
+      <a href="#" @click.prevent="$router.push('/posts/1')" class="btn btn-outline-info btn-sm">Show</a>
       <a href="#" class="btn btn-outline-warning btn-sm">Edit</a>
       <a href="#" class="btn btn-outline-danger btn-sm">Delete</a>
     </div>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <PostDetail />
+</template>
+
+<script>
+import PostDetail from '@/components/post/PostDetail';
+export default {
+  components: {
+    PostDetail,
+  },
+};
+</script>
