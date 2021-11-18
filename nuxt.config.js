45ff2525589005796469203366108f39555c35d9
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-18 18:06:53 +0300

    Use asyncData and loading bar in Nuxt.js property

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#666' },
 
   /*
    ** Global CSS
--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,21 @@ export default {
     PostList,
     About,
   },
-  data() {
+  /*  data() {
     return {
-      fetchedPosts: [
-        { id: 1, title: 'Nuxt.js', subTitle: 'I learn Nuxt.js', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', author: 'Kemal Öncel' },
-        { id: 2, title: 'Node.js', subTitle: 'I learn Node.js', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', author: 'Kemal Öncel' },
-      ],
+      fetchedPosts: [],
     };
+  }, */
+  created() {},
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        fetchedPosts: [
+          { id: 1, title: 'Nuxt.js', subTitle: 'I learn Nuxt.js', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', author: 'Kemal Öncel' },
+          { id: 2, title: 'Node.js', subTitle: 'I learn Node.js', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', author: 'Kemal Öncel' },
+        ],
+      });
+    }, 2000);
   },
 };
 </script>
