d2dd8edd817723af4185685a3d52db22f3be065c
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-18 10:56:52 +0300

    Create the Admin Layout and make the Admin Page

---
+++ pages/admin/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container d-flex justify-content-center align-items-center">
+      <button class="btn btn-dark">Add a new post</button>
+    </div>
+    <PostList />
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/post/PostList';
+export default {
+  layout: 'admin',
+  components: {
+    PostList,
+  },
+};
+</script>
+
+<style scoped></style>
