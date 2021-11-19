e4aedf22c1e1cbc9fc375ccdd7cee7133a16c1cc
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-19 10:36:18 +0300

    use fetch method

--- pages/index.vue
+++ pages/index.vue
@@ -14,29 +14,24 @@ export default {
     About,
   },
   created() {
-    this.$store.dispatch('setPosts', this.fetchedPosts);
+    // this.$store.dispatch('setPosts', this.fetchedPosts);
   },
-  asyncData(context) {
+  computed: {
+    fetchedPosts() {
+      return this.$store.getters.getPosts;
+    },
+  },
+  fetch(context) {
     return new Promise((resolve, reject) => {
-      resolve({
-        fetchedPosts: [
+      resolve(
+        context.store.dispatch('setPosts', [
           { id: 1, title: 'Nuxt.js', subTitle: 'I learn Nuxt.js', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', author: 'Kemal Öncel' },
           { id: 2, title: 'Node.js', subTitle: 'I learn Node.js', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', author: 'Kemal Öncel' },
-        ],
-      });
-    })
-      .then((result) => {
-        return result;
-      })
-      .catch((err) => {
-        context.error(new Error());
-      });
-    /*   callback(null, {
-      fetchedPosts: [
-        { id: 1, title: 'Nuxt.js', subTitle: 'I learn Nuxt.js', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', author: 'Kemal Öncel' },
-        { id: 2, title: 'Node.js', subTitle: 'I learn Node.js', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', author: 'Kemal Öncel' },
-      ],
-    }); */
+        ])
+      );
+    }).catch((err) => {
+      context.error(new Error());
+    });
   },
 };
 </script>
